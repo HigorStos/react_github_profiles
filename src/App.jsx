d11a856a3bf8141aecff010fb30b30ca6796e735
Higor Santos <higor.stos@outlook.com>
2023-09-07 23:22:38 -0300

    fix: form

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
+
 import Profile from "./components/Profile"
 import ReposList from "./components/ReposList";
 
 function App() {
   const [user, setUser] = useState('');
+  const [input, setInput] = useState('');
 
   return (
     <>
       <div className='container'>
         <form className='form'>
           <input
-            onBlur={e => setUser(e.target.value)}
+            onBlur={e => setInput(e.target.value)}
             className='formInput'
             type="text"
             placeholder='GitHub User'
           />
-          <button className='formButton' type="button">Search</button>
+          <button
+            onClick={() => setUser(input)}
+            className='formButton'
+            type="button"
+          >
+              Search
+          </button>
         </form>
       </div>
 
